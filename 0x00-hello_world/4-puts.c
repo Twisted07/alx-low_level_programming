376c854535dcfd2a0acdacdd91ee4ebbf9505507
Twisted <iamtwisted07@gmail.com>
2022-04-22 13:56:45 +0100

    C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+*main - function begins
+*
+* Return: 0, function ends
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
