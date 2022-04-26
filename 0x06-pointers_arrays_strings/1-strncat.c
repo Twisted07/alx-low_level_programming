d5d79055703bf0df3a94deb1437097cb801b84ce
twisted07 <iamtwisted07@gmail.com>
2022-04-26 06:11:16 +0100

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,14 +14,12 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0, j = 0;
 
 	while (dest[i] != '\0')
-	{
 		i++;
-	}
 
 	while (src[j] != '\0' && j < n)
 		dest[i] = src[i];
-		i++;
-		j++;
+			i++;
+			j++;
 
 	dest[i] = '\0';
 	return (0);
