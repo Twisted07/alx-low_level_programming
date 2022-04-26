7f01dd2fdf236557627430ddf305ae987ef8e426
twisted07 <iamtwisted07@gmail.com>
2022-04-26 18:44:22 +0100

    Pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: string to evaluate.
+ *
+ * Return: The length of @i.
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
