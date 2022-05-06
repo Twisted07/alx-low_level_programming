f47b9570928e60f3b67b179df491232b17a57197
twisted07 <iamtwisted07@gmail.com>
2022-05-06 17:12:56 +0100

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - Program prints out its name
+*@argc: counts the argument
+*@argv: pointer to array of arguments
+* Return: 0 on success
+*/
+
+int main(int argc, char *argv[])
+{
+	print("%s\n", argv[0]);
+	return (0);
+}
+
