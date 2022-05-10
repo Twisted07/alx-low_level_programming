075a2c73915bfd164f67487b72ca901b5524e248
twisted07 <iamtwisted07@gmail.com>
2022-05-10 12:01:45 +0100

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,31 +3,23 @@
 
 /**
 *_strdup - function returns a pointer to the duplicated string
-*@str:
-*@
+*@str: strin
 *Return: Null for failure
 */
 
 char *_strdup(char *str)
 {
-	int i = 0;
+
 	char *dup_str;
-	int len;
 
 	if (str == NULL)
 		return (NULL);
 
-	dup_str = malloc(sizeof(char) * len);
+	dup_str = malloc(sizeof(char));
 
 	if (dup_str == NULL)
 		return (NULL);
 
-	i = 0;
-	while (i < len)
-	{
-		*(dup_str + i) = *(str + i);
-		i++;
-	}
-
-	return (dup_str);
+	else
+		return (dup_str);
 }
