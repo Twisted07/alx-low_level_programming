7c75341081ca60f7a7032d8df5c6895f7867faac
twisted07 <iamtwisted07@gmail.com>
2022-05-24 23:59:48 +0100

    list header

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,20 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-/**
-*list_s - defines elements of list
-*@*str: pointer to string
-*@len: data
-*/
-
-
-typedef struct list_s
-{
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+#include "list.h"
 
 
 
