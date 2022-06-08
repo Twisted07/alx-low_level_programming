720b1ff8329af60d2ba0d13121796b49c912e089
twisted07 <iamtwisted07@gmail.com>
2022-06-08 23:38:18 +0100

    macro updated

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,9 +3,4 @@
 
 #define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
 
-#endif#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-
-#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
-
 #endif
