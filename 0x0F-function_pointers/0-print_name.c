a7bc51915411c2e1916e4d73d3314f96af3b278e
twisted07 <iamtwisted07@gmail.com>
2022-06-09 17:43:46 +0100

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
-*print_name - execute a function given as a parameter
- * @name: parameter that function ptr needs
- * @f:function to execute
-*/
-
-void print_name(char *name, void (*f)(char *))
-{
-	if (name != NULL && f != NULL)
-		f(name);
-}
+#ifndef SIZE
+#define SIZE 1024
+#endif
