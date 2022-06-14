78b14015577c38fa09294b5e81707461a5647bdd
twisted07 <iamtwisted07@gmail.com>
2022-06-14 13:15:15 +0100

    dog header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef "DOG_H"
+#define DOG_H
+
+/**
+* struct dog - structure of dog type
+* @name: first member of the type (a pointer)
+* which stores the address to the value of dog name
+* @owner: stores the address to the value of dog owner
+* @age: stores the value of dog age and accepts float data type
+*/
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
